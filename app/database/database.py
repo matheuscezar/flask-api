bd68f5dd1d0a18fd013e21dfbd0fb46aa570dc76
root <matheuscezar@hotmail.com>
2020-11-30 14:30:10 -0300

    editing works with docker

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.ext.declarative import declarative_base  
 
-db = create_engine('postgresql://postgres:masterkey@db:5432/flaskapi')
+db = create_engine('postgresql://postgres:masterkey@db:5432/postgres')
 metadata = MetaData(bind=db)
 
 base = declarative_base()
